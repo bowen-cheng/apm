2af19a0d56bcc3a2220f7ceedf82decf7d5da547
Bowen Cheng <blueicycky@gmail.com>
2017-03-10 23:37:20 -0500

    Implement product filter using custom pipe

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,10 @@
+import { ProductFilterPipe } from './products/product-filter.pipe';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from "./products/product-list.component";
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   imports: [
@@ -12,7 +13,8 @@ import { ProductListComponent } from "./products/product-list.component";
   ],
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductFilterPipe
   ],
   bootstrap: [ AppComponent ]
 })
