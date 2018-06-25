ccab4f656b29a25282465000db53ccd720323a03
Bowen Cheng <blueicycky@gmail.com>
2018-06-25 17:50:55 -0400

    Transform data display using built-in pipes from Angular

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -14,7 +14,7 @@ export class ProductListComponent {
     {
       'productId': 1,
       'productName': 'Leaf Rake',
-      'productCode': 'GDN-0011',
+      'productCode': 'gdn-0011',
       'releaseDate': 'March 19, 2016',
       'description': 'Leaf rake with 48-inch wooden handle.',
       'price': 19.95,
@@ -24,7 +24,7 @@ export class ProductListComponent {
     {
       'productId': 2,
       'productName': 'Garden Cart',
-      'productCode': 'GDN-0023',
+      'productCode': 'gdn-0023',
       'releaseDate': 'March 18, 2016',
       'description': '15 gallon capacity rolling garden cart',
       'price': 32.99,
@@ -34,7 +34,7 @@ export class ProductListComponent {
     {
       'productId': 5,
       'productName': 'Hammer',
-      'productCode': 'TBX-0048',
+      'productCode': 'tbx-0048',
       'releaseDate': 'May 21, 2016',
       'description': 'Curved claw steel hammer',
       'price': 8.9,
@@ -44,7 +44,7 @@ export class ProductListComponent {
     {
       'productId': 8,
       'productName': 'Saw',
-      'productCode': 'TBX-0022',
+      'productCode': 'tbx-0022',
       'releaseDate': 'May 15, 2016',
       'description': '15-inch steel blade hand saw',
       'price': 11.55,
@@ -54,7 +54,7 @@ export class ProductListComponent {
     {
       'productId': 10,
       'productName': 'Video Game Controller',
-      'productCode': 'GMG-0042',
+      'productCode': 'gmg-0042',
       'releaseDate': 'October 15, 2015',
       'description': 'Standard two-button video game controller',
       'price': 35.95,
