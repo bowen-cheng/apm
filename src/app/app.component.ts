7a2279b6ca8db747f4934c9689519fc2e64f6e31
Bowen Cheng <blueicycky@gmail.com>
2018-06-30 14:03:17 -0400

    Register ProductService with AppComponent (the root component)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  /*
+  - Providers are defined in Angular component or Angular module metadata
+  - ProductService is the provider since it returns ProductService class (Self is provider)
+  - ProductService is now available to AppComponent and all of its nested components
+  */
+  providers: [ProductService]
 })
 export class AppComponent {
   title = 'Acme Product Management';
