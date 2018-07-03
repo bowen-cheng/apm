34ce972e1b9ecc3ec869b9bc128f1bb76f0d8013
Bowen Cheng <blueicycky@gmail.com>
2018-07-03 16:00:34 -0400

    Generate new ProductDetailsComponent using Angular CLI
    ng g c products/product-detail.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductDetailComponent,
     ProductListComponent,
     StarComponent
   ],
