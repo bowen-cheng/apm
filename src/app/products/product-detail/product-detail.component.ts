77343297d67e7fa9436d5b7f737922ae396727a7
Bowen Cheng <blueicycky@gmail.com>
2018-07-07 08:44:41 -0400

    Activate route with code
    The route parameters are passed by calling an Angular function.

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
@@ -15,15 +15,21 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: Product;
 
-  constructor(private route: ActivatedRoute, private _productService: ProductService) {
+  constructor(private activatedRoute: ActivatedRoute,
+              private _productService: ProductService,
+              private router: Router) {
   }
 
   ngOnInit() {
     // The '+' symbol is a JS shortcut for converting strings to numbers
-    const id: number = +this.route.snapshot.paramMap.get('id');
+    const id: number = +this.activatedRoute.snapshot.paramMap.get('id');
 
     this._productService.getProducts().subscribe((products: Product[]) => {
       this.product = products.filter(value => value.productId === id).pop();
     });
+  }
+
+  onBack(): void {
+    this.router.navigate(['products']);
   }
 }
