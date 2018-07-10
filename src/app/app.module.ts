7e044948596141ebd4eea9ee5968aff0a11150eb
Bowen Cheng <blueicycky@gmail.com>
2018-07-10 09:36:03 -0400

    Group Product related modules and classes into product feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,26 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
-import { ProductDetailGuard } from './products/product-detail/product-detail.guard';
-import { ProductListComponent } from './products/product-list/product-list.component';
-import { StarComponent } from './shared/star.component';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductDetailComponent,
-    ProductListComponent,
-    WelcomeComponent,
-    StarComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      {
-        path: 'products/:id',
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent
-      },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    ]),
+    ProductsModule
   ],
   // Registering a service in Angular modules is deprecated since V6
   // providers: [ProductDetailGuard],
